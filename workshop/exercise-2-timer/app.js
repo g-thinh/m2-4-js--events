bb23eab982afa25ae2e3ecce78310ec0d16c3941
Thinh Nguyen <thinh-nguyen@hotmail.com>
2020-05-02 20:21:41 -0400

    ex2 added current time and stopwatch

--- workshop/exercise-2-timer/app.js
+++ workshop/exercise-2-timer/app.js
@@ -10,17 +10,109 @@ function addZero(i) {
   return i;
 }
 
+
 setInterval(function () {
-  //assign current datetime
   let today = new Date();
+  let hours = addZero(today.getHours());
+  let minutes = addZero(today.getMinutes());
+  let seconds = addZero(today.getSeconds());
 
-  //use the .getHours(), .getMinutes(), and .getSeconds() method to return
-  //the respective time values from the variable _today_ and nest it inside
-  //the addZero() function for formating
-  let time = 
-    addZero(today.getHours()) + ":" 
-    + addZero(today.getMinutes()) + ":" 
-    + addZero(today.getSeconds());
+  let sectionA = document.getElementById('s1');
 
-  // console.log(time);
+  // console.log(sectionA);
+  sectionA.innerText = 
+    `${hours}` + ":" +
+    `${minutes}` + ":" +
+    `${seconds}`;
+
 },1000)
+
+
+
+// ######################### STOP WATCH #####################################
+
+let intervalStart;
+
+function start() {
+
+  //set default start time to 00:00:00
+  let hours = 0;
+  let minutes = 0;
+  let seconds = 0;
+
+  intervalStart = setInterval(function () {
+
+    //always count seconds
+    seconds++;
+
+    //once 60 seconds, add 1 minute and reset seconds
+    if(seconds == 60) {
+      seconds = 0;
+      minutes++;
+    
+    //once 60 minutes, add 1 hour and reset minutes
+    } else if (minutes == 60) {
+      minutes = 0;
+      hours++;
+    }
+  
+    let sectionB = document.getElementById('s2');
+  
+    // console.log(sectionA);
+    sectionB.innerText = 
+      `${addZero(hours)}` + ":" +
+      `${addZero(minutes)}` + ":" +
+      `${addZero(seconds)}`;
+
+  },1000)
+  
+}
+
+function stop() {
+
+  clearInterval(intervalStart);
+
+  //set default start time to 00:00:00
+  let hours = 0;
+  let minutes = 0;
+  let seconds = 0;
+
+  let sectionB = document.getElementById('s2');
+
+  // console.log(sectionA);
+  sectionB.innerText = 
+    `${addZero(hours)}` + ":" +
+    `${addZero(minutes)}` + ":" +
+    `${addZero(seconds)}`;
+
+}
+
+
+//add event listener to the start button
+document.getElementById("start").addEventListener('click', start);
+document.getElementById("stop").addEventListener('click', stop);
+
+
+
+// #################### RANDOM STUFF THAT WORKS ##############################
+
+//create a function that returns the current time
+let timer = function() {
+
+  //use a setInterval() function to create a timer
+  setInterval(function () {
+
+    //assign current datetime
+    let today = new Date();
+
+    //use the .getHours(), .getMinutes(), and .getSeconds() method to return
+    //the respective time values from the variable _today_ and nest it inside
+    //the addZero() function for formating
+    let time = 
+      addZero(today.getHours()) + ":" 
+      + addZero(today.getMinutes()) + ":" 
+      + addZero(today.getSeconds());
+
+    console.log(time);
+  },1000)
+}
