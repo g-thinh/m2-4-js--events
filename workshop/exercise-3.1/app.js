9ba98519659a6eba803c1423cba1d3232881c8fa
Thinh Nguyen <thinh-nguyen@hotmail.com>
2020-05-03 15:32:16 -0400

    ex3.1 button grid ready

--- workshop/exercise-3.1/app.js
+++ workshop/exercise-3.1/app.js
@@ -1,7 +1,24 @@
-let main = document.querySelector('main')
 
-main.classList.add('main');
+//Create a process that will add all 20 buttons
+for(let i=0; i<20; i++) {
 
-main.classList
+  //select the main tag
+  let main = document.querySelector('main');
 
+  //create a div tag
+  let box = document.createElement('div');
+  box.classList.add('btn');
 
+  //insert the <div> into <main>
+  main.appendChild(box);
+  
+
+  //create a button tag
+  let button = document.createElement('button');
+  button.innerText = 'Click Me';
+
+  //insert the <button> into the <div> tag
+  box.appendChild(button);
+  
+  console.log(i);
+}
