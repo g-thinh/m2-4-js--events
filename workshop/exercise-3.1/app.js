82607d16f8be5dbf97574dc7b4473d7d94b49adb
Thinh Nguyen <thinh-nguyen@hotmail.com>
2020-05-03 15:39:26 -0400

    ex3.1 completed, js added

--- workshop/exercise-3.1/app.js
+++ workshop/exercise-3.1/app.js
@@ -1,3 +1,4 @@
+// ###################### CREATE 20 BUTTONS ##################################
 
 //Create a process that will add all 20 buttons
 for(let i=0; i<20; i++) {
@@ -20,5 +21,19 @@ for(let i=0; i<20; i++) {
   //insert the <button> into the <div> tag
   box.appendChild(button);
   
-  console.log(i);
+  // console.log(i);
 }
+
+// ################## CHANGE COLOR UPON CLICK #################################
+
+
+//create a function to change the background color of a button element to green
+
+let buttons = document.querySelectorAll('button');
+
+buttons.forEach(button => {
+  button.addEventListener('click', function() {
+    button.style.backgroundColor = 'green';
+    button.style.color = 'white';
+  })
+})
